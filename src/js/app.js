5d063982c55ef038031a0620d6390f7ed1aa4411
stormozov <web@stormozov.ru>
2025-02-27 18:42:45 +0300

    Загружаю файлы сайта для gh-pages

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import '../scss/app.scss';
-
-/* JS components */
-import './components/smooth-scrolling';
-import './components/hamburger';
-import './components/add-map-review';
-import './components/get-current-year';
